c1245d9e3a02a07ffcc8b5ce5bbef541e08f1956
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2023-05-08 19:29:38 -0300

    feat: adicao de um tipo de teste diferente para corrigir o erro do flake8

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -99,7 +99,10 @@ def test_decorar_relatorio():
     report = ColoredReport(SimpleReport)
     assert (
         report.generate(data)
-        == """\033[32mData de fabricação mais antiga:\033[0m \033[36m2020-09-06\033[0m
-\033[32mData de validade mais próxima:\033[0m \033[36m2023-09-17\033[0m
-\033[32mEmpresa com mais produtos:\033[0m \033[31mTarget Corporation\033[0m"""
+        == "\033[32mData de fabricação mais antiga:\033[0m "
+        + "\033[36m2020-09-06\033[0m" + "\n"
+        + "\033[32mData de validade mais próxima:\033[0m "
+        + "\033[36m2023-09-17\033[0m" + "\n"
+        "\033[32mEmpresa com mais produtos:\033[0m "
+        + "\033[31mTarget Corporation\033[0m"
     )
