4b2a3530dd3e0638eee0ed3f597983c6836b24ac
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2023-05-08 14:39:57 -0300

    feat: testando criacao da instacia da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,15 @@
 # from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1,
+        "Camisa básica",
+        "nike",
+        "25/02/2023",
+        "25/02/2029",
+        155587,
+        "na sombra",
+    )
+    assert isinstance(new_product, Product)
