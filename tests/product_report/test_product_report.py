bf4053597b54da666ac976f8acc819a864a53625
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2023-05-08 18:58:20 -0300

    feat: teste  do product reporty implementado

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,12 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1, "Camisa", "nike", "02-02-2023", "02-04-2029", 11234, "na sombra"
+    )
+    assert (
+        new_product.__repr__()
+        == "O produto Camisa fabricado em 02-02-2023 por nike com " +
+        "validade até 02-04-2029 precisa ser armazenado na sombra."
+    )
